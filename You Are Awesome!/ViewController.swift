680e12d38149e70494931bfc588a42eabdb8ddf7
Anthony Lundberg <tonylundberg12@gmail.com>
2020-09-16 13:01:06 -0700

    2.12 complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -25,15 +25,19 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive",
                         "I can't wait to download your app!"]
         
-        messageLabel.text = messages[index]
-      
+        messageLabel.text = messages.randomElement()!
         
-        if index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
         
+        
+//        messageLabel.text = messages[index]
+//
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+//
 //        let message1 = "You Are Da Bomb!"
 //        let message2 = "You Are Great!"
 //        let message3 = "You Are Amazing!"
