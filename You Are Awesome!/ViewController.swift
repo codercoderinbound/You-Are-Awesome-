e7c3a7ce7e83b9ce4bbacf133f9fec381d6d40aa
Anthony Lundberg <tonylundberg12@gmail.com>
2020-09-16 11:31:51 -0700

    2.10 completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -11,19 +13,40 @@ class ViewController: UIViewController {
 
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Da Bomb!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
+        let messages = ["You Are Awesome!!",
+                        "You Are Great!",
+                        "You Are Fantastic",
+                        "When the Genius Bar needs help, they call you",
+                        "You Brighten My Day!",
+                        "You are da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive",
+                        "I can't wait to download your app!"]
+        
+        messageLabel.text = messages[index]
+      
+        
+        if index == messages.count - 1 {
+            index = 0
+        } else {
+            index = index + 1
         }
-        else if messageLabel.text == message2 {
-            messageLabel.text = message3
-        }
-        else {
-            messageLabel.text = message1
-        }
+        
+//        let message1 = "You Are Da Bomb!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        }
+//        else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        }
+//        else {
+//            messageLabel.text = message1
+//        }
         
     }
     
