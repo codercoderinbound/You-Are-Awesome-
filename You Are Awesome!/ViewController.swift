f6e1dd8d0d0e0845ae199e1528f63dd968ec1d2b
Anthony Lundberg <tonylundberg12@gmail.com>
2020-09-22 14:53:01 -0700

    2.20 complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var soundSwitch: UISwitch!
     var awesomePlayer = AVAudioPlayer()
     var index = -1
     var imageIndex = -1
@@ -13,25 +14,19 @@ class ViewController: UIViewController {
     let numOfSounds = 5
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func playSound(soundName: String) {
         if let sound = NSDataAsset(name: soundName) {
-            //check to see if sound.data is sound file
             do {
                 try awesomePlayer = AVAudioPlayer(data: sound.data)
                 awesomePlayer.play()
             } catch {
-                // if sound.data is not viable audio file
                 print("ERROR: data in \(soundName) couldn't be played as a sound.")
             }
         } else {
             print("ERROR: file \(soundName) disn't load")
         }
-        //play a sound
-        var soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName)
     }
     func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
         var newIndex: Int
@@ -39,8 +34,14 @@ class ViewController: UIViewController {
             newIndex = Int.random(in: 0..<maxValue)
         } while lastNumber == newIndex
     
-        index = newIndex
+        return newIndex
     }
+    
+    @IBAction func soundSwitchedPressed(_ sender: UISwitch) {
+        if soundSwitch.isOn == false && soundIndex != -1 {
+                awesomePlayer.stop()
+            }
+        }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
         
@@ -55,55 +56,21 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive",
                         "I can't wait to download your app!"]
         
-        
-        // OR you can do this vvv
-        // var newIndex = -1
-        
-        //show a message
         index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         messageLabel.text = messages[index]
         
-        //show an image
         imageIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numOfImages)
         awesomeImageView.image = UIImage(named: "img\(imageIndex)")
         
-        //get random number to use in soundName file
-        soundIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numOfSounds)
+        if soundSwitch.isOn {
+            soundIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numOfSounds)
+//            let soundName = "sound\(soundIndex)"
+            playSound(soundName: "sound\(soundIndex)")
+        }
         
-        
-//        messageLabel.text = messages.randomElement()!
-//
-//
-        
-//        messageLabel.text = messages[index]
-//
-//
-//        if index == messages.count - 1 {
-//            index = 0
-//        } else {
-//            index = index + 1
-//        }
-//
-//        let message1 = "You Are Da Bomb!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        }
-//        else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        }
-//        else {
-//            messageLabel.text = message1
-//        }
-        
+        //  playSound() (above) needs to go inside a conditional 'if' statement once you've added the switch (below)
     }
     
-
-        
     
-    
-
-
+    // put IBAction for UISwitch here
 }
